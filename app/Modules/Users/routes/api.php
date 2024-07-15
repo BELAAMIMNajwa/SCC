68da18cdbb7ab60e2fb50c25d93b4319e7e5c874
Najwa <najwabelaamim@gmail.com>
2024-07-15 12:11:20 +0100

    route user

--- app/Modules/Users/routes/api.php
+++ app/Modules/Users/routes/api.php
@@ -1,4 +1,36 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Modules\Users\Http\Controllers\UsersController;
+
+
+
+
+
+// Routes publiques (non protégées par 'auth:sanctum')
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'api/users'
+], function () {
+    Route::post('/login', [UsersController::class, 'login']);
+    Route::post('/register', [UsersController::class, 'create']);
+});
+
+// Routes protégées par 'auth:sanctum'
+Route::group([
+    'middleware' => 'auth:sanctum',
+    'prefix' => 'api/users'
+], function () {
+    Route::post('/logout', [UsersController::class, 'logout']);
+    Route::get('/', [UsersController::class, 'index']);
+    Route::get('/{id}', [UsersController::class, 'get']);
+    Route::post('/create', [UsersController::class, 'create']);
+    Route::post('/update', [UsersController::class, 'update']);
+    Route::post('/delete', [UsersController::class, 'delete']);
+    Route::post('/changePassword', [UsersController::class, 'changePassword']);
+    Route::post('/resetPassword', [UsersController::class, 'resetPassword']);
+    Route::post('/createAll', [UsersController::class, 'createAll']);
+
+
+
+});
